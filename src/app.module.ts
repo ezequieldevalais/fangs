9ba189ffd913f5d2b52591a990f4f945afbc994d
Ezequiel de Valais <ezequiel.devalais@gmail.com>
2024-02-21 14:49:15 -0300

    dockerizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { AppointmentController } from './appointment/appointment.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.19.0.2',
+      port: 5432,
+      password: 'fangspassword',
+      username: 'fangs',
+      entities: [User],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule],
+  controllers: [AppController, AppointmentController],
   providers: [AppService],
 })
 export class AppModule {}
