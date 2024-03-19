1f37f956aaf6ebe8c6a2a9d7e62502c73a9c5a6b
Ezequiel de Valais <ezequiel.devalais@gmail.com>
2024-03-19 17:57:02 -0300

    add pet service and controller

--- src/modules/pet/pet.controller.ts
+++ src/modules/pet/pet.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { PetService } from './pet.service';
 
 @Controller('pet')
 export class PetController {
+  constructor(private readonly petService: PetService) {}
+
   @Get('/cat')
   getCat(): string {
-    return 'this is a cat';
+    return this.petService.getCat();
   }
 
   @Get('/dog')
   getDog(): string {
-    return 'this is a cat';
+    return this.petService.getDog();
   }
 }
